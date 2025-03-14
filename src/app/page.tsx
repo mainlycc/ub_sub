d3914c1026003ef7cfe61d23113fb6021616559f
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-03-14 14:38:04 +0100

    porządek1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,12 @@
 "use client"
 
 import React from 'react';
-import Link from 'next/link';
 import InsuranceCalculator from '@/components/InsuranceCalculator';
 import InsuranceTypeCards from '@/components/InsuranceTypeCards';
 import AboutUs from '@/components/AboutUs';
 import FAQ from '@/components/FAQ';
-import { Button } from "@/components/ui/button";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const HomePage = (): React.ReactNode => {
-  // Dodaję komentarz "eslint-disable-next-line" żeby uniknąć błędu nieużywanych importów
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const unusedComponentsReference = {
-    InsuranceCalculator,
-    InsuranceTypeCards,
-    AboutUs,
-    FAQ,
-    Button,
-    NavigationMenu,
-    Link
-  };
-
   return (
     <>
       {/* Kalkulator - importowany jako komponent */}
