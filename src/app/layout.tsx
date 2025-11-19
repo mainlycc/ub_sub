4e00281b2af5a60cd40daf2d86f0f0affd243910
Stanislaw Blicharski <mainly.agn@gmail.com>
2025-11-19 10:12:05 +0100

    dodanie hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,21 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Facebook Pixel Code */}
+        {/* Hotjar Tracking Code for gapauto */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6580007,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.className} bg-[#EAE7FC] min-h-screen flex flex-col m-0 p-0`}>
         <Navbar />
